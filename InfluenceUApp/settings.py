48cde4329bef79b99982911d5e46d6b9551b728e
Anthony Bachour <anthonybachour@gmail.com>
2017-02-08 15:25:02 +0000

    stopped browser redirect

--- InfluenceUApp/settings.py
+++ InfluenceUApp/settings.py
@@ -22,6 +22,8 @@ SECRET_KEY = '$6(x*g_2g9l_*g8peb-@anl5^*8q!1w)k&e&2!i)t6$s8kia94'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
+PREPEND_WWW = False
+
 # Application definition
 
 INSTALLED_APPS = (
@@ -130,7 +132,7 @@ STATICFILES_FINDERS = (
 )
 
 # change to true when in production!!!
-COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', True)
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 TEMPLATES = [
     {
